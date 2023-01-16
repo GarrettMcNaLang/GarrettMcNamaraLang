475451307a6c69e3519cd17471afd1deb4290303
GarrettMcNaLang <112025126+GarrettMcNaLang@users.noreply.github.com>
2023-01-15 19:41:11 -0500

    playermove/playerjump/needscamerafollow

--- jumpscareforest/Assets/scripts/playerMovement.cs
+++ jumpscareforest/Assets/scripts/playerMovement.cs
@@ -10,7 +10,12 @@ public class playerMovement : MonoBehaviour
 
     private bool isGrounded;
     public Transform FeetPos;
+    public float JumpForce;
     public float Radius;
+    public LayerMask isGround;
+    private float JumpTimeCounter;
+    public float jumpTime;
+    private bool isJumping;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,5 +27,42 @@ public class playerMovement : MonoBehaviour
     {
         movementInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(movementInput * speed, rb.velocity.y);
+
+        isGrounded = Physics2D.OverlapCircle(FeetPos.position, Radius, isGround);
+
+        if (movementInput > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+
+        if(movementInput < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * JumpForce;
+            isJumping = true;
+            JumpTimeCounter = jumpTime;
+            Debug.Log(0);
+        }
+
+        if(Input.GetKey(KeyCode.Space) && isJumping == true)
+        {
+            if(JumpTimeCounter > 0)
+            {
+                rb.velocity = Vector2.up * JumpForce;
+                JumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+            }
+            
+        }
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            isJumping = false;
+        }
     }
 }
