eb88ca49669ef0e0261fff1e803eeb75f48448b4
GarrettMcNaLang <112025126+GarrettMcNaLang@users.noreply.github.com>
2023-01-16 23:35:06 -0500

    Startedcamerafollow

--- jumpscareforest/Assets/scripts/CameraFollow.cs
+++ jumpscareforest/Assets/scripts/CameraFollow.cs
@@ -1,18 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //the character
+    public GameObject Target;
+    //how much distance the character can move freely before our camera starts moving
+    public Vector2 Offset;
+    //defined boundary box
+    public Vector2 threshold;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //formula that calculates threshold
+        private Vector3 calculateThreshold()
+        {
+            //defines aspect ratio of camera
+            Rect aspect = Camera.main.pixelRect;
+        }
     }
 }
