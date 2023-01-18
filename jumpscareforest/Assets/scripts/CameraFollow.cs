6bbee1da4d884aa3ea6ee8b24a031041da50ce48
GarrettMcNaLang <112025126+GarrettMcNaLang@users.noreply.github.com>
2023-01-18 11:40:03 -0500

    platformsscript/camerafollowdone

--- jumpscareforest/Assets/scripts/CameraFollow.cs
+++ jumpscareforest/Assets/scripts/CameraFollow.cs
@@ -1,4 +1,5 @@
-
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
@@ -8,15 +9,60 @@ public class CameraFollow : MonoBehaviour
     //how much distance the character can move freely before our camera starts moving
     public Vector2 Offset;
     //defined boundary box
-    public Vector2 threshold;
+    private Vector2 threshold;
+    //defines how fast the camera moves
+    public float speed;
+    //reference to Rigibody
+    private Rigidbody2D rb;
 
-    void Update()
+     void Start()
     {
-        //formula that calculates threshold
-        private Vector3 calculateThreshold()
+        //defines threshold
+        threshold = calculateThreshold();
+        rb = Target.GetComponent<Rigidbody2D>();
+
+    }
+
+    void FixedUpdate()
+    {
+        //define the distance the file object is from the center of the camera
+        Vector2 follow = Target.transform.position;
+        //keeps track of the distance the player is from the center of the x-axis
+        float xDifference = Vector2.Distance(Vector2.right * transform.position.x, Vector2.right * follow.x);
+        float yDifference = Vector2.Distance(Vector2.up * transform.position.y, Vector2.up * follow.y);
+
+        //camera will remain where currently is
+        Vector3 newPosition = transform.position;
+        if(Mathf.Abs(xDifference) >= threshold.x)
         {
-            //defines aspect ratio of camera
-            Rect aspect = Camera.main.pixelRect;
+            newPosition.x = follow.x;
         }
+        if(Mathf.Abs(yDifference) >= threshold.y)
+        {
+            newPosition.y = follow.y;
+        }
+        //determines speed of barrier if the player speed exceeds said barrier; if true it goes max velocity, if false it goes the default speed
+        float moveSpeed = rb.velocity.magnitude > speed ? rb.velocity.magnitude : speed;
+        //makes it so the camera will move towards this position each frame
+        transform.position = Vector3.MoveTowards(transform.position, newPosition, moveSpeed * Time.deltaTime);
+        
+    }
+    //formula that calculates threshold
+    private Vector3 calculateThreshold()
+    {
+        //defines aspect ratio of camera
+        Rect aspect = Camera.main.pixelRect;
+        //defines dimensions of camera
+        Vector2 t = new Vector2(Camera.main.orthographicSize * aspect.width / aspect.height, Camera.main.orthographicSize);
+        //defines threshold(subtract boundary box value by offset value)
+        t.x -= Offset.x;
+        t.y -= Offset.y;
+        return t;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Vector2 border = calculateThreshold();
+        Gizmos.DrawWireCube(transform.position, new Vector3(border.x * 2, border.y * 2, 1));
     }
 }
